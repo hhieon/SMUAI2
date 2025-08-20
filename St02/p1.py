8eade3d667923e7b58335bb0a9fc4da918c9b2b3
hhieon <nahyeon3897@naver.com>
2025-08-20 11:30:29 +0900

    20250820 11:30

--- St02/p1.py
+++ St02/p1.py
@@ -1,3 +1,30 @@
 import streamlit as st
 
+# Sidebar
+st.sidebar.markdown("Clicked Page 1")
+st.sidebar.markdown("Side Menu")
+st.sidebar.button("Click")
+st.sidebar.radio("성별", ["여자", "남자"])
+
+# Page
 st.title("Page 1")
+st.markdown("Mark Down")
+st.header("Header")
+st.subheader("Sub Header")
+st.caption("Caption")
+st.code("""
+def func():
+    print("OK!")
+def func2():
+    print("OK@")
+""", language="python")
+
+st.code("""
+#include <stdio.h>
+
+int main() {
+    printf("혜주");
+
+    return 0;
+}
+""", language="c")
